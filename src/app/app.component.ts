d74685bd022d79260dec6e2c8589d9dff8d1dcb6
Bormash1991 <53615723+Bormash1991@users.noreply.github.com>
2022-12-01 22:08:55 +0200

    ls_14 add html and scss as in the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { TypeOfProduct } from './models/TypeOfProduct.inteface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'hys_angular';
+  protected date: TypeOfProduct[] = this.createRandomeDate(8);
+
+  createRandomeDate(n: number = 1): TypeOfProduct[] {
+    let str: string, price: number;
+    let mass: TypeOfProduct[] = [];
+    for (let i = 0; i < n; i++) {
+      str = Math.random().toString(36).substring(2);
+      price = Math.random() * 3000 - 1000 + 1000;
+      mass.push({
+        id: i + 1,
+        name: str,
+        price: price,
+      });
+    }
+    return mass;
+  }
 }
---
+++ src/app/models/TypeOfProduct.inteface.ts
@@ -0,0 +1,5 @@
+export interface TypeOfProduct {
+  id: number;
+  name: string;
+  price: number;
+}
