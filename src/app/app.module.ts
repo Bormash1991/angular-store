09fdcda758c06d1139365a8d6b504e9432b53e16
Bormash1991 <53615723+Bormash1991@users.noreply.github.com>
2022-12-08 01:41:24 +0200

    ls_16 first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+
 import { ProductsRoutingModule } from './products/products-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { ProductsModule } from './products/products.module';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule, SharedModule],
 })
 export class ProductsModule {}
