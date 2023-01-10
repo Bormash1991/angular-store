02746c54e336085001a377fe062f523ccefd21cd
Bormash1991 <53615723+Bormash1991@users.noreply.github.com>
2023-01-10 18:25:07 +0200

    deleted console.log

--- src/app/admin-panel/orders/orders.component.ts
+++ src/app/admin-panel/orders/orders.component.ts
@@ -32,7 +32,6 @@ export class OrdersComponent implements OnInit, OnDestroy {
           this.loading$.next(false);
           this.orders = this.filterService.setData(data, 5);
           this.ordersLength = data.length;
-          console.log(data);
         }
       },
       error: (error) => {},
